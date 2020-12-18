172d175821cbcbe7225818f8f2cf0fb83129f80b
ramshanadeem <rimshanadeem64@gmail.com>
2020-12-18 16:39:55 +0500

    owais

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var PORT = process.env.PORT || 5000;
+var express = require("express");
+var server = express();
+server.get("/", (req, res, next) => {
+    console.log("some one get manue ");
+    res.send("manue : what do you want to get ? food or water !");
+
+})
+
+
+server.get("/water", (req, res, next) => {
+    console.log("some one get manue ");
+    res.send("HERE IS WATER");
+
+})
+server.listen(PORT, () => {
+    console.log("server is running on port 3000");
+})
